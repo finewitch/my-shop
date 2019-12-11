4494db9e20d60f9689964bf87fe98e640ad1f4bf
finewitch <finewitch@gmail.com>
2019-12-11 19:15:51 +0100

    prisma permissions

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,18 +1,31 @@
 const Mutations = {
-  createItem(parent, args, context, info){
+  async createItem(parent, args, context, info){
         
-        const item = context.db.mutation.createItem({
+        const item = await context.db.mutation.createItem({
             data:{
                 ...args
             }
         }, info)
-        .then((response)=>{
-            return response
-        })
-
-        console.log(item, 'item')
-
         return item
+    },
+
+    updateItem(parent, args, context, info){
+        const updates = {...args}
+        delete updates.id 
+        return context.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+                }
+            }, info
+        )
+    },
+    async deleteItem(parent, args, context, info){
+        const where = {id: args.id}
+
+        const item = await context.db.query.item({ where }, `{id title}`)
+
+        return context.db.mutation.deleteItem({ where }, info)
     }
 
 };
